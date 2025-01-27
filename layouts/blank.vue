0c18a966d16851507dbdec2688f44735ce0c7704
Sachin <sy945877@gmail.com>
2025-01-27 12:59:21 +0530

    renamed all js file names

---
+++ layouts/blank.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "blank",
+  name: "url-shortner",
+});
+</script>
---
+++ pages/u/[id].vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+    Sample U page {{ id }}
+</div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+console.log(id)
+</script>
